1634d514d139ec1266ba33f602ed49057072de50
Brayan Lipe <blipe@unsa.edu.pe>
2022-07-13 01:49:53 -0500

    ÂAdding new features to Budget class and creating the GUI menu

---
+++ src/com/spitfire/gui/MenuDemo.java
@@ -0,0 +1,11 @@
+package com.spitfire.gui;
+
+import javax.swing.*;
+
+public class MenuDemo {
+    private JPanel panelMenu;
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
+    private JButton startButton;
+    private JButton createAccountButton;
+}
--- src/com/spitfire/model/Budget.java
+++ src/com/spitfire/model/Budget.java
@@ -4,20 +4,27 @@ public class Budget {
     int budgetID;
     double fixedIncome;
     double variableIncome;
+    double totalIncome;
     double fixedExpense;
     double variableExpense;
+    double totalExpense;
+    double myBudget;
+    double totalSaving;
     int periodTime;
 
-    public Budget(int budgetID, double fixedIncome, double variableIncome,
-                  double fixedExpense, double variableExpense,
-                  int periodTime) {
+    /*public Budget(int budgetID, double fixedIncome, double variableIncome,
+                  double totalIncome, double fixedExpense, double variableExpense,
+                  double totalExpense, double totalSaving, int periodTime) {
         this.budgetID = budgetID;
         this.fixedIncome = fixedIncome;
         this.variableIncome = variableIncome;
+        this.totalIncome = totalIncome;
         this.fixedExpense = fixedExpense;
         this.variableExpense = variableExpense;
+        this.totalExpense = totalExpense;
+        this.totalSaving = totalSaving;
         this.periodTime = periodTime;
-    }
+    }*/
 
     public int getBudgetID() {
         return budgetID;
@@ -43,6 +50,11 @@ public class Budget {
         this.variableIncome = variableIncome;
     }
 
+    public double getMyTotalIncome () { return totalIncome; }
+
+    public void setMyTotalIncome(double totalIncome) { totalIncome = this.fixedIncome + this.variableIncome;
+        this.totalIncome = totalIncome; }
+
     public double getFixedExpense() {
         return fixedExpense;
     }
@@ -59,11 +71,46 @@ public class Budget {
         this.variableExpense = variableExpense;
     }
 
-    public int getPeriodTime() {
-        return periodTime;
+    public double getTotalExpense() {
+        return totalExpense;
     }
+
+    public void setTotalExpense() {
+        this.totalExpense = this.variableExpense + this.fixedExpense;
+    }
+
+    public double getMyBudget() { return myBudget; }
+
+    public void setMyBudget() { this.myBudget = this.totalIncome - this.totalExpense; }
+
+    public double getMySaving() { return totalSaving; }
+
+    public void setMySaving(int percentage, double priceDesirable, int periodTime) {
+        double p = (this.totalIncome*percentage)/100;
+        if (this.myBudget >= p) {
+            double savingPerMonth = priceDesirable/periodTime;
+            this.totalSaving = priceDesirable;
+            System.out.println("You must save S/." + savingPerMonth + " in " + periodTime + " months");
+        }
+        else {
+            System.out.println("Save your money please");
+        }
+    }
+
+    public int getPeriodTime() { return periodTime; }
 
     public void setPeriodTime(int periodTime) {
         this.periodTime = periodTime;
+    }
+}
+class TestBudgetDrive {
+    public static void main(String[] args) {
+        Budget b1 = new Budget();
+        double ti = 0;
+        b1.setBudgetID(1);
+        b1.setFixedIncome(125.5);
+        b1.setVariableIncome(50.0);
+        b1.setMyTotalIncome(ti);
+        System.out.println(b1.getMyTotalIncome());
     }
 }
